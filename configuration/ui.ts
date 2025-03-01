801226919fc3e20cceb2f39c3ea30e854876e2d4
PavKumar52 <pavithra.mk9352@gmail.com>
2025-03-01 15:40:41 -0500

    BlueMBA Buddy Page Design

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `Copyright 2025 Pavithra; Use at your risk`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const CHAT_HEADER: string = `Welcome to BlueMBA Buddy!`;
+export const MESSAGE_PLACEHOLDER: string = `Type your MBA program query here...`;
+export const FOOTER_MESSAGE: string = `Copyright 2025 UNC Kenan-Flagler Business School`;
+export const CLEAR_BUTTON_TEXT: string = `Start Over`;
+export const PAGE_TITLE: string = `BlueMBA Buddy`;
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, friendly AI assistant of ${OWNER_NAME} of ${OWNER_DESCRIPTION}. He's your first TarHeel friend and guide, helping you navigate your MBA journey`;
 
-export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
+export const EMPTY_CITATION_MESSAGE: string = "No specific reference available for this information";
