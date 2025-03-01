c1950baf92d6745d1a56c380d5d148ea52cac404
PavKumar52 <pavithra.mk9352@gmail.com>
2025-03-01 17:07:33 -0500

    BlueMBA identity updated

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Full-Time MBA program`;
 export const OWNER_DESCRIPTION: string = `UNC Kenan-Flagler Business School`;
 
 export const AI_NAME: string = `BlueMBA Buddy`;
-export const AI_TONE: string = `[Friendly and Supportive]`;
-export const AI_ROLE: string = `[Guide and Mentor]`;
+export const AI_TONE: string = `Friendly and Supportive`;
+export const AI_ROLE: string = `Guide and Mentor`;
